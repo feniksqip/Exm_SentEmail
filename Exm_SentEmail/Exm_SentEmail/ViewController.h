d1a3595fabae3b097da5e9bf79c42c2101b7a607
Admin <admin@Mac-Admin.local>
2015-01-09 16:36:40 +0300

    Create form.

---
+++ Exm_SentEmail/Exm_SentEmail/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  Exm_SentEmail
+//
+//  Created by Admin on 09.01.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+- (IBAction)sentMain:(id)sender;
+
+@end
+
---
+++ Exm_SentEmail/Exm_SentEmail/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Exm_SentEmail
+//
+//  Created by Admin on 09.01.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)sentMain:(id)sender {
+}
+@end
